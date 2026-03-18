d1b0c5d60be1fe08cf1245ae28f1de00efcc17e3
efp-bsc-com-01-24-dotcom <efp-bsc-com-01-24@unima.ac.mw>
2026-03-18 12:07:01 +0200

    add question 2

--- subscription.cpp
+++ subscription.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
 
 using namespace std;
 
 int main(){
+    // srand(0);
+    // cout<<rand()<<endl;
 
-
+   
 
 
     return 0;
