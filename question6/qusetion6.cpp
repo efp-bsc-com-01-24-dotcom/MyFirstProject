f9c1be43ea9cf835329349c08789b2d988b6e6f8
efp-bsc-com-01-24-dotcom <efp-bsc-com-01-24@unima.ac.mw>
2026-03-30 10:15:04 +0200

    modified question6.cpp

--- question6/qusetion6.cpp
+++ question6/qusetion6.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-int consonuntvowels(const string& text){
+int countvowels(string text){
     int count = 0;
     for(char c : text){
          c = tolower(c);
@@ -29,25 +29,39 @@ int countwords(string text){
     return count;
 }
 
-string reverse(string text){
+string reverseText(string text){
     string reversed ="";
-    for(int i = text.length()-1;1>=0;i++){
+    for(int i = text.length()-1;i>=0;i--){
         reversed += text[i];
     }
     return reversed; 
 }
+string capitalizeSecondLatter(string text){
+    stringstream ss(text);
+    string word,result="";
+
+    
+    while(ss >> word){
+        if(ss>>word){
+            word[1] = toupper(word[1]);
+        }
+        result += word + " ";
+    }
+    return result;
+}
 
 int main(){
-    string myText;
-    string fileData;
-    string newstring = "*";
-
+    
     ifstream MyReadFile("file.txt");
+    string fileData;
 
-   
-    
-    
-    cout<<newstring;
+    getline(MyReadFile, fileData);
+    MyReadFile.close();
+    cout<<"Original text: "<<fileData<<endl;
+    cout<<"Number of words: "<<countwords(fileData)<<endl;
+    cout<<"Reversed text: "<<reverseText(fileData)<<endl;
+    cout<<"Number of vowels: "<<countvowels(fileData)<<endl;
+    cout<<"capitalized text: "<<capitalizeSecondLatter(fileData)<<endl;
 
 
 } 
